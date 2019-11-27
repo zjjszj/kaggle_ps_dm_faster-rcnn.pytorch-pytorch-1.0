397be564e7562e5bfe8a31d353d7a562e78e57c2
zhangjin <1411605960+zjjszj@users.noreply.github.com>
2019-11-27 09:29:07 +0800

    modify compile path because of path too long

--- model/roi_layers/roi_align.py
+++ model/roi_layers/roi_align.py
@@ -5,7 +5,9 @@ from torch.autograd import Function
 from torch.autograd.function import once_differentiable
 from torch.nn.modules.utils import _pair
 
-from mydetector.model import _C
+#from mydetector.model import _C
+from mydetector import _C
+
 
 import pdb
 
